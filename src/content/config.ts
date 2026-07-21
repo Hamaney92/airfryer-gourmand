5c84597093ae793b9b0e8ca275c53db5f9612224
Hamaney92 <youssef.hamane.00@gmail.com>
2026-07-21 22:30:56 +0200

    Add files via upload

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,6 +15,7 @@ const recipes = defineCollection({
     servings: z.number().default(2),
     temp: z.string(),
     image: z.string().optional(),
+    alt: z.string().optional(),
     quickAnswer: z.string(),
     ingredients: z.array(z.string()).default([]),
     steps: z.array(z.string()).default([]),
